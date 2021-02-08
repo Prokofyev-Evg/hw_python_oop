ad5a4a30ef02ec3b4df93baf4deb05b511a1aff1
Евгений <Prokofyev.Evg@yandex.ru>
2021-02-09 00:14:42 +0300

    Edited to abide PEP8 rules

--- homework.py
+++ homework.py
@@ -13,15 +13,19 @@ class Calculator:
         return sum(r.amount for r in self.records if r.date == dt.date.today())
 
     def get_week_stats(self):
+        return sum(r.amount for r in self.records if self.in_last_week(r.date))
+
+    def in_last_week(self, date):
         week_ago = dt.date.today() - dt.timedelta(days=7)
-        return sum(r.amount for r in self.records if r.date <= dt.date.today() and r.date > week_ago)
+        return date <= dt.date.today() and date > week_ago
 
     def get_balance_for_today(self):
         return self.limit - self.get_today_stats()
 
 
 class Record:
-    def __init__(self, amount, comment, date=dt.date.today().strftime('%d.%m.%Y')):
+    def __init__(self, amount, comment,
+                 date=dt.date.today().strftime('%d.%m.%Y')):
         self.amount = amount
         self.comment = comment
         self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
@@ -34,9 +38,10 @@ class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         balance = self.get_balance_for_today()
         if balance > 0:
-            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {balance} кКал'
+            return ('Сегодня можно съесть что-нибудь ещё,'
+                    f' но с общей калорийностью не более {balance} кКал')
         else:
-            return f'Хватит есть!'
+            return 'Хватит есть!'
 
 
 class CashCalculator(Calculator):
@@ -49,14 +54,14 @@ class CashCalculator(Calculator):
         if balance > 0:
             return f'На сегодня осталось {value:.2f} {ending}'
         elif balance == 0:
-            return f'Денег нет, держись'
+            return 'Денег нет, держись'
         else:
             return f'Денег нет, держись: твой долг - {-value:.2f} {ending}'
-    
+
     def convert_currency(self, rub_value, currency):
-        if currency == 'usd': 
+        if currency == 'usd':
             return rub_value / self.USD_RATE, 'USD'
-        if currency == 'eur': 
+        if currency == 'eur':
             return rub_value / self.EURO_RATE, 'Euro'
         else:
             return rub_value, 'руб'
